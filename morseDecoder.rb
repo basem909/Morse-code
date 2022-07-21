9939812c631366be852158adb65abf463c8d5107
Bassem <bassem@MacBook-Pro-de-Bassem.local>
2022-07-21 01:06:53 -0300

    Add a method to decode a word and a phrase in morse code

--- morseDecoder.rb
+++ morseDecoder.rb
@@ -41,5 +41,20 @@ def decode_char(char)
  return morse_dict[char]
 end
 
+def decode_word (str)
+  string = str.split(' ')
+  arr = []
+  string1 = string.each {|i| arr.push("#{decode_char(i)}")}
+  string2 = arr.join(' ')
+  return string2
+ end
 
+def decode_phrase (str)
+  phrase = str.split('   ')
+  arr = []
+  phrase1 = phrase.each {|i| arr.push("#{decode_word(i)}")}
+  phrase2 = arr.join('   ')
+  return phrase2
+end
 
+decode_phrase ('.... . .-.. .-.. ---   .-- --- .-. .-.. -..')
